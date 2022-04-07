d56629240362d60ef78d7f438f3b3e2b9fe17939
rjakkinappali-ontic <rjakkinappali@ontic.co>
2022-04-07 16:42:00 +0530

    Forgot request mapping

--- src/main/java/com/example/kafkaReplyRequest/controller/KafkaTestController.java
+++ src/main/java/com/example/kafkaReplyRequest/controller/KafkaTestController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.ExecutionException;
 
 @RestController
+@RequestMapping("/testing")
 public class KafkaTestController {
 
     @Autowired
